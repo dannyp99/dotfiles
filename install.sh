227373ac21473dc2b92c1341421726780d3a00ab
Danny Pires <dannypires99@gmail.com>
2024-11-25 22:43:41 -0500

    Update for config files

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ DOT_CONFIG="$HOME/.config"
 echo "-----------------STARTING: .config symlinks-----------------"
 
 ln -sf "$PWD/config/nvim" "$DOT_CONFIG/nvim" && echo "Neovim Symlink Created"
+ln -sf "$PWD/config/btop" "$DOT_CONFIG/btop" && echo "BTOP Symlink Created"
+ln -sf "$PWD/config/alacritty" "$DOT_CONFIG/alacritty" && echo "Alacritty Symlink Created"
 ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig" && echo "Gitconfig Symlink Created"
 ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf" && echo "Tmux Symlink Created"
 ln -sf "$PWD/zsh/.zshrc" "$HOME/.zshrc" && echo ".zshrc Symlink Created"
