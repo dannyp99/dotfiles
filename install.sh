f1a37d3f35bb2d7a2e792ef4e8d33b5fc7678792
Danny Pires <dannypires99@gmail.com>
2024-12-04 23:24:51 -0500

    Add dotfile info from github

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "-----------------STARTING: tmux tpm clone-----------------"
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    echo "tpm is alread installed skipping..."
+fi
+echo "-----------------COMPLETED: tmux tpm clone-----------------"
+echo ""
+
+DOT_CONFIG="$HOME/.config"
+echo "-----------------STARTING: .config symlinks-----------------"
+
+if [[ ! -d "$DOT_CONFIG/nvim" ]];then
+    ln -sf "$PWD/config/nvim" "$DOT_CONFIG/nvim" && echo "Neovim Symlink Created"
+else
+    echo "NEOVIM Symlink Found Skipping..."
+fi
+if [[ ! -d "$DOT_CONFIG/alacritty" ]];then
+    ln -sf "$PWD/config/alacritty" "$DOT_CONFIG/alacritty" && echo "Alacritty Symlink Created"
+else
+    echo "Alacritty Symlink Found Skipping..."
+fi
+if [[ ! -d "$DOT_CONFIG/btop" ]];then
+    ln -sf "$PWD/config/btop" "$DOT_CONFIG/btop" && echo "BTOP Symlink Created"
+else
+    echo "Btop Symlink Found Skipping..."
+fi
+ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig" && echo "Gitconfig Symlink Created"
+ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf" && echo "Tmux Symlink Created"
+ln -sf "$PWD/zsh/.zshrc" "$HOME/.zshrc" && echo ".zshrc Symlink Created"
+ln -sf "$PWD/.bashrc" "$HOME/.bashrc" && echo ".bashrc Symlink Created"
+
+echo "-----------------COMPLETED: .config symlinks-----------------"
+
