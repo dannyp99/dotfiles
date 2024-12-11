ee55fadbaba3c8f400b72c81d3965facaa6ceba6
Danny Pires <dannypires99@gmail.com>
2024-12-10 22:16:49 -0500

    Move .zshrc items around

--- install.sh
+++ install.sh
@@ -33,11 +33,12 @@ else
 fi
 ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig" && echo "Gitconfig Symlink Created"
 ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf" && echo "Tmux Symlink Created"
-ln -sf "$PWD/zsh/.zshrc" "$HOME/.zshrc" && echo ".zshrc Symlink Created"
 ln -sf "$PWD/.bashrc" "$HOME/.bashrc" && echo ".bashrc Symlink Created"
 
 echo "-----------------COMPLETED: .config symlinks-----------------"
 printf "\n"
 echo "-----------------STARTING: zsh custom install-----------------"
+touch "$HOME/.zshrc"
 ./zsh/zshCustomInstall.sh
+ln -sf "$PWD/zsh/.zshrc" "$HOME/.zshrc" && echo ".zshrc Symlink Created"
 echo "-----------------COMPLETED: zsh custom install-----------------"
