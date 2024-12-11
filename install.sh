6d1bde01e30708d0bdc61600498f553df5113fbc
Danny Pires <dannypires99@gmail.com>
2024-12-10 22:24:40 -0500

    Update tpm update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "-----------------STARTING: tmux tpm clone-----------------"
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 else
     echo "tpm is alread installed skipping..."
 fi
