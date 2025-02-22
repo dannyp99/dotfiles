9d8fb8c52ca023dd4c71f947ba6e529216c8b9f8
dannyp99 <dannypires99@gmail.com>
2025-02-22 14:42:41 -0500

    Updated keybindings for copilot

--- config/nvim/lua/danny/lazy/copilot.lua
+++ config/nvim/lua/danny/lazy/copilot.lua
@@ -3,5 +3,7 @@ return
     'github/copilot.vim',
     setup = function ()
         vim.g.copilot_enabled = 1
+        vim.keymap.set('i', '<C-.>', '<Plug>(copilot-next)', { noremap = false })
+        vim.keymap.set('i', '<C-,>', '<Plug>(copilot-previous)', { noremap = false })
     end,
 };
