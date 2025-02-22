5e2ea38547e03d9af4602047e7dc106f10c18b70
dannyp99 <dannypires99@gmail.com>
2025-02-22 16:18:09 -0500

    Comment out remappings

--- config/nvim/lua/danny/lazy/copilot.lua
+++ config/nvim/lua/danny/lazy/copilot.lua
@@ -3,7 +3,11 @@ return
     'github/copilot.vim',
     setup = function ()
         vim.g.copilot_enabled = 1
-        vim.keymap.set('i', '<C-.>', '<Plug>(copilot-next)', { noremap = false })
-        vim.keymap.set('i', '<C-,>', '<Plug>(copilot-previous)', { noremap = false })
+        -- vim.keymap.set('i', '<C-M-]>', '<Plug>(copilot-next)')
+        -- vim.keymap.set('i', '<C-M-[>', '<Plug>(copilot-previous)')
+        -- vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
+        -- vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-line)')
+        
     end,
+    
 };
